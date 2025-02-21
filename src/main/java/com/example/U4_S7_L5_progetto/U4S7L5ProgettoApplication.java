c02f0e6a6e3791a413b66b75086229168edfae73
notdav04 <davide.alonzi26@gmail.com>
2025-02-21 14:57:27 +0100

    setup struttura

---
+++ src/main/java/com/example/U4_S7_L5_progetto/U4S7L5ProgettoApplication.java
@@ -0,0 +1,13 @@
+package com.example.U4_S7_L5_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4S7L5ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4S7L5ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U4_S7_L5_progetto/U4S7L5ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U4_S7_L5_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4S7L5ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
